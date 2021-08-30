fdefdd803fd0edd0435d5728aebdced5de3a1ba7
Tyler Haglund <relyt4me@gmail.com>
2021-08-30 14:31:58 -0600

    Create object of resume info to be used in page render

---
+++ src/data/resumeData.js
@@ -0,0 +1,73 @@
+export const resumeData = {
+  resumeURL: "https://docs.google.com/document/d/1P4Ed4BhXaKmbXTCznYf3UTh8r0feYnZeMS65tNQCqLU/edit?usp=sharing",
+  gitHubURL: "https://github.com/relyt4me",
+  linkedInURL: "https://www.linkedin.com/in/tyler-haglund/",
+  bioDescription: "My name is Tyler Fincher he/him, and I am a passionate problem solver, JavaScript developer, military veteran, and quick learner. I have honed my skills in React, Css, GraphQL, HTML and other languages with the fundamentals of object-oriented programing, test driven development, and an agile workflow. I find drive in building applications that are accessible for variety of users from different backgrounds and with different capabilities. In my work I look for chances to implement creative solutions while working in teams to continue learning from others and sharing my own knowledge.",
+  education: [{
+    school: "Turing School of Software & Design",
+    position: "Graduate Front End Web Development Program",
+    time: "April 2020 - November 2020",
+    description: [
+      "Completed 13 different software development projects using vanilla JS, React, Apollo, Redux and more",
+      "Through each project focused on a test driven workflow with test suites in Mocha/Chai and React testing library",
+      "Worked on solo projects, paired groups, and larger teams up to 6 including mixed front and back end students",
+      "Built knowledge through a strong focus early on the fundamentals of JavaScript",
+      "Finished with 1,400 hours of experience learning new frameworks, languages and libraries, followed by implementation in multiple projects",
+    ]
+  },
+  {
+    school: "Fort Gordon School US Army",
+    position: "Graduate MultiChannel Transmission Operator and Maintainer",
+    time: "March 2012 - October 2012",
+    description: [
+      "Completed rigorous training for on new radio and satellite technology",
+      "Developed unique problem solving skills and fortitude through rigorous military tests", 
+      "Learned through joint signal force training the fundamentals of network creation and signal flow",
+      "Acquired critical leadership tools and teamwork skills for working in fast paced and stressful environments",
+      "Granted Secret Clearance"
+    ]
+  }],
+  jobs: [
+    {
+      name: "G6 Helpdesk",
+      position: "Front desk software and hardware support",
+      time: "January 2015 - December 2019",
+      description: [
+        "Assisted 5000 Soldiers and staff in software and hardware request through a network ticketing system",
+        "Managed web permissions through SharePoint and Active Directory",
+        "Maintained network security for the Colorado National Guard force",
+        "Provided maintenance on computers, servers, printers, laptops and more for mission readiness",
+        "Worked with civilian, officer, and soldier alike to provide quality care for all network members",
+      ]
+    },
+    {
+      name: "540th Network Support Company",
+      position: "Range Extension Unit Sergeant",
+      time: "October 2012 - June 2019",
+      description: [
+        "Supervised 8 Soldiers to plan and implement radio and satellite transmissions for the Colorado National Guard during missions domestic and abroad",
+        "Maintained a network of communication terminals and vehicles using IP management sna d server-side packet transmission",
+        "Kept soldiers of unit in positive moral, physical, and technical readiness for a rapid response to unique and difficult challenges",
+      ]
+    },
+  ],
+  techSkills: [
+    "JavaScript",
+    "React",
+    "React Testing Library",
+    "React Hooks",
+    "React Router",
+    "Apollo",
+    "Apollo Hooks",
+    "Chai / Mocha",
+    "Webpack",
+    "Redux",
+    "CSS",
+    "Sass",
+    "Node.js",
+    "Graph QL",
+    "Express.js",
+    "HTML5",
+    "Secret Clearance",
+  ]
+}
