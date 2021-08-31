6f2a89123cbd0ea3a6855306567fe4518398c959
Tyler Haglund <relyt4me@gmail.com>
2021-08-31 11:42:17 -0600

    Put all float icons in js for import

---
+++ src/components/Home/float-icons/float-icons.js
@@ -0,0 +1,17 @@
+import cheese from "./cheese.png";
+import computer from "./computer.png";
+import cooking from "./cooking.png";
+import dice from "./dice.png";
+import flag from "./flag.png";
+import mountain from "./mountain.png";
+import tent from "./tent.png";
+
+export const floatIcons = [
+  { image: cheese, name: "cheese" },
+  { image: computer, name: "computer" },
+  { image: cooking, name: "cooking pot" },
+  { image: dice, name: "d20 dice" },
+  { image: flag, name: "rainbow pride flag" },
+  { image: mountain, name: "mountain" },
+  { image: tent, name: "tent" },
+];
