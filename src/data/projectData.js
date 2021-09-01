b605fce308eccf48df085ecf37b7e4bf99d90e65
Tyler Haglund <relyt4me@gmail.com>
2021-09-01 16:09:02 -0600

    import images inside of project data

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,28 +1,23 @@
+import fridgeImg from "../assets/fridgeglass.png";
+import leomundImg from "../assets/leomund.png";
+
 export const projectData = [
   {
     name: "Fridge to Glass",
-    stack: [
-      "React",
-      "React Router",
-      "React Testing Library",
-      "Redux",
-    ],
+    stack: ["React", "React Router", "React Testing Library", "Redux"],
     repoURL: "https://github.com/relyt4me/stretch-project",
     liveURL: "https://fridge-to-glass.vercel.app/",
-    description: "A cocktail searching site where a user can filter and search for cocktails by the ingredients they may already have at home. Each cocktail comes with a recipe and users may even look to a random recipe if that suites them.",
-    screenshot: "../assets/fridgeglass.png",
+    description:
+      "A cocktail searching site where a user can filter and search for cocktails by the ingredients they may already have at home. Each cocktail comes with a recipe and users may even look to a random recipe if that suites them.",
+    screenshot: fridgeImg,
   },
   {
-    name: "Fridge to Glass",
-    stack: [
-      "React",
-      "React Router",
-      "Jest",
-      "Restful API"
-    ],
+    name: "Leomund's Superhut",
+    stack: ["React", "React Router", "Jest", "Restful API"],
     repoURL: "https://github.com/relyt4me/leomunds-superhut",
     liveURL: "https://leomunds-superhut.herokuapp.com/",
-    description: "A shopping tool app used to assists the Game masters for Dungeons and Dragons. Allows for the user to browse, search and add items to a cart that tallies total a total converted gold value. The user can also manipulate the total cost giving discounts or applying taxes.",
-    screenshot: "../assets/leomund.png",
-  }
-]
+    description:
+      "A shopping tool app used to assists the Game masters for Dungeons and Dragons. Allows for the user to browse, search and add items to a cart that tallies total a total converted gold value. The user can also manipulate the total cost giving discounts or applying taxes.",
+    screenshot: leomundImg,
+  },
+];
