7ebd0b095e7baca0f30a368ef258b2c60bdc3395
Tyler Haglund <relyt4me@gmail.com>
2021-09-20 14:23:21 -0600

    Print technical skills as list

---
+++ src/components/TechnicalSkills/TechnicalSkills.js
@@ -0,0 +1,19 @@
+import "./TechnicalSkills.css";
+import { resumeData } from "../../data/resumeData";
+
+const TechnicalSkills = () => {
+  const printSkills = () => {
+    return resumeData.techSkills.map((skill) => {
+      return <li>{skill}</li>;
+    });
+  };
+
+  return (
+    <div className="technical-skills">
+      <h1>Technical Skills</h1>
+      <div className="tech-skill-list">{printSkills()}</div>
+    </div>
+  );
+};
+
+export default TechnicalSkills;
