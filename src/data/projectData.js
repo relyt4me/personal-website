f34e2b27bce617395d0d84174ac730a12a9b7bf3
Tyler Haglund <relyt4me@gmail.com>
2021-09-23 11:47:20 -0600

    Fixed minor grammer issues after peer review

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -8,7 +8,7 @@ export const projectData = [
     repoURL: "https://github.com/relyt4me/stretch-project",
     liveURL: "https://fridge-to-glass.vercel.app/",
     description:
-      "A cocktail searching site where a user can filter and search for cocktails by the ingredients they may already have at home. Each cocktail comes with a recipe and users may even look to a random recipe if that suites them.",
+      "A cocktail searching site where a user can filter and search for cocktails by the ingredients they may already have at home. Each cocktail comes with a recipe and users may even look to a random recipe if that suits them.",
     screenshot: fridgeImg,
   },
   {
@@ -17,7 +17,7 @@ export const projectData = [
     repoURL: "https://github.com/relyt4me/leomunds-superhut",
     liveURL: "https://leomunds-superhut.herokuapp.com/",
     description:
-      "A shopping tool app used to assists the Game masters for Dungeons and Dragons. Allows for the user to browse, search and add items to a cart that tallies total a total converted gold value. The user can also manipulate the total cost giving discounts or applying taxes.",
+      "A shopping tool app used to assist the Game masters for Dungeons and Dragons. Allows for the user to browse, search and add items to a cart that tallies a total converted gold value. The user can also manipulate the total cost giving discounts or applying taxes.",
     screenshot: leomundImg,
   },
 ];
--- src/data/resumeData.js
+++ src/data/resumeData.js
@@ -8,7 +8,7 @@ export const resumeData = {
   linkedInURL: "https://www.linkedin.com/in/tyler-haglund/",
   emailURL: "relyt4me@gmail.com",
   bioDescription:
-    "My name is Tyler Fincher he/him, and I am a passionate problem solver, JavaScript developer, military veteran, and quick learner. I have honed my skills in React, Css, GraphQL, HTML and other languages with the fundamentals of object-oriented programing, test driven development, and an agile workflow. I find drive in building applications that are accessible for variety of users from different backgrounds and with different capabilities. In my work I look for chances to implement creative solutions while working in teams to continue learning from others and sharing my own knowledge.",
+    "My name is Tyler Fincher (he/him), and I am a passionate problem solver, JavaScript developer, military veteran, and quick learner. I have honed my skills in React, CSS, GraphQL, HTML and other languages with the fundamentals of object-oriented programming, test driven development, and an agile workflow. I find drive in building applications that are accessible for a variety of users from different backgrounds and with different capabilities. In my work I look for chances to implement creative solutions while working in teams to continue learning from others and sharing my own knowledge.",
   education: [
     {
       name: "Turing School of Software & Design",
@@ -16,7 +16,7 @@ export const resumeData = {
       time: "April 2020 - November 2020",
       description: [
         "Completed 13 different software development projects using vanilla JS, React, Apollo, Redux and more",
-        "Through each project focused on a test driven workflow with test suites in Mocha/Chai and React testing library",
+        "Ensured each project focused on a test driven workflow with test suites in Mocha/Chai and React testing library",
         "Worked on solo projects, paired groups, and larger teams up to 6 including mixed front and back end students",
         "Built knowledge through a strong focus early on the fundamentals of JavaScript",
         "Finished with 1,400 hours of experience learning new frameworks, languages and libraries, followed by implementation in multiple projects",
@@ -27,7 +27,7 @@ export const resumeData = {
       position: "Graduate MultiChannel Transmission Operator and Maintainer",
       time: "March 2012 - October 2012",
       description: [
-        "Completed rigorous training for on new radio and satellite technology",
+        "Completed rigorous training on new radio and satellite technology",
         "Developed unique problem solving skills and fortitude through rigorous military tests",
         "Learned through joint signal force training the fundamentals of network creation and signal flow",
         "Acquired critical leadership tools and teamwork skills for working in fast paced and stressful environments",
@@ -54,7 +54,7 @@ export const resumeData = {
       time: "October 2012 - June 2019",
       description: [
         "Supervised 8 Soldiers to plan and implement radio and satellite transmissions for the Colorado National Guard during missions domestic and abroad",
-        "Maintained a network of communication terminals and vehicles using IP management sna d server-side packet transmission",
+        "Maintained a network of communication terminals and vehicles using IP management and server-side packet transmission",
         "Kept soldiers of unit in positive moral, physical, and technical readiness for a rapid response to unique and difficult challenges",
       ],
     },
